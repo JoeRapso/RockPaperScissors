7782bf03197d2e13bfc1db7ea2b54fb22c622616
joerapso <joe.rapso@gmail.com>
2024-10-22 15:14:13 +0100

    New version of game

--- ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
+++ ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a0554815b9141fca661c8e6e7ab740e9d68ab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MiniGame/Program.cs
+++ MiniGame/Program.cs
@@ -2,22 +2,6 @@
 using ClassLibrary.Menu;
 using ClassLibrary.UserInput;
 
-/* feedback
--Move choices should be an Enum rather than magic strings.
--Separation of IO and logic is good to see.
--Absolutely no comments in the code. At the very least classes should be commented with what they do.
--Explicit declaration of variable types is poor practice.
--Absolutely no OOP. While not strictly necessary for this project, there's no evidence of knowledge of inheritance or interfaces.
--It should be possible to type for example "rock" or "r" instead of "1".
--Random should be a private static field in the classes it is used in. Otherwise, a new Random object is being instantiated on each method call.
--This line of code: `var winner = gameWins.OrderByDescending(pair => pair.Value).Take(1).ToDictionary(pair => pair.Key, pair => pair.Value);` is very inefficient and wordy.
--After each round there should be some feedback on who won.
--The program crashes at the end of the game due to an invalid API key. Using OpenAI is a nice idea but errors need to be properly handled.
--Tests are correct but very basic. There should be some edge cases and tests that are expected to fail."
-*/
-
-//List<string> menuChoices = new List<string>() { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
-
 Enums.MenuChoices menuChoices;
 
 Dictionary<string, int> gameChoiceFrequency = new Dictionary<string, int>()
